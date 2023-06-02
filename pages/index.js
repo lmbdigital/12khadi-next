4a3bf1cf40c552ca96e2732e1e278411234c5f37
lmbdigital <135345137+lmbdigital@users.noreply.github.com>
2023-06-02 22:19:23 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,486 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <table style="text-align: center; border: 3px solid #ff8000;">
+<tr style="background-color: green; color: white;"> <td align="center" colspan="12"><b>Barah Khadi &#8211; बारह खड़ी</b>
+</td></tr>
+<tr style="background-color: #ffcc99;">
+     <td>&#8211;<br>&#8211;</td>
+     <td> ा<br>a</td>
+     <td> ि<br>i</td>
+     <td> ी<br>ee</td>
+     <td>ु<br>u</td>
+     <td>ू<br>oo</td>
+     <td>े<br>e</td>
+     <td>◌ै<br>ai</td>
+     <td>ो<br>o</td>
+     <td> ौ <br>au</td>
+     <td>ं<br>an</td>
+     <td>ः<br>ah</td>
+   </tr>
+  <tr style="background-color: #f2f2f2;">
+  <td>क <br />ka</td>
+  <td>का <br />ka</td>
+  <td>कि <br />ki</td>
+  <td>की <br />kee</td>
+  <td>कु <br />ku</td>
+  <td>कू <br />koo</td>
+  <td>के <br />ke</td>
+  <td>कै <br />kai</td>
+  <td>को <br />ko</td>
+  <td>कौ <br />kau</td>
+  <td>कं <br />kan</td>
+  <td>कः <br />kah </td>
+</tr>
+<tr style="background-color: #ffcc99;">
+<td>ख <br />kha</td>
+<td>खा <br />kha</td>
+<td>खि <br />khi</td>
+<td>खी <br />khee</td>
+<td>खु <br />khu</td>
+<td>खू <br />khoo</td>
+<td>खे <br />khe</td>
+<td>खै <br />khai</td>
+<td>खो <br />kho</td>
+<td>खौ <br />khau</td>
+<td>खं <br />khan</td>
+<td>खः <br />khah</td>
+</tr>
+<tr style="background-color: #f2f2f2;">
+<td>ग <br />ga</td>
+<td>गा <br />ga</td>
+<td>गि <br />gi</td>
+<td>गी <br />gee</td>
+<td>गु <br />gu</td>
+<td>गू <br />goo</td>
+<td>गे <br />ge</td>
+<td>गै <br />gai</td>
+<td>गो <br />go</td>
+<td>गौ <br />gau</td>
+<td>गं <br />gan</td>
+<td>गं <br />gah</td>
+</tr>
+<tr style="background-color: #ffcc99;">
+ <td>घ <br />gha</td>
+ <td>घा <br />gha</td>
+<td>घि <br />ghi</td>
+<td>घी <br />ghee</td>
+<td>घु <br />ghu</td>
+<td>घू <br />ghoo</td>
+<td>घे <br />ghe</td>
+<td>घै <br />ghai</td>
+<td>घो <br />gho</td>
+<td>घौ <br />ghau</td>
+<td>घं <br />ghan</td>
+<td>घः <br />ghah</td>
+</tr>
+<tr style="background-color: #f2f2f2;">
+<td>च <br />cha</td>
+<td>चा <br />cha</td>
+<td>चि <br />chi</td>
+<td>ची <br />chee</td>
+<td>चु <br />chu</td>
+<td>चू <br />choo</td>
+<td>चे <br />che</td>
+<td>चै <br />chai</td>
+<td>चो <br />cho</td>
+<td>चौ <br />chau</td>
+<td>चं <br />chan</td>
+<td>चः <br />chah</td>
+</tr>
+<tr style="background-color: #ffcc99;">
+<td>छ <br />chha</td>
+<td>छा <br />chha</td>
+<td>छि <br />chhi</td>
+<td>छी <br />chhee</td>
+<td>छु <br />chhu</td>
+<td>छू <br />chhoo</td>
+<td>छे <br />chhe</td>
+<td>छै <br />chhai</td>
+<td>छो <br />chho</td>
+<td>छौ <br />chhau</td>
+<td>छं <br />chhan</td>
+<td>छः <br />chhah</td>
+</tr>
+<tr style="background-color: #f2f2f2;">
+<td>ज <br />ja</td>
+<td>जा <br />ja</td>
+<td>जि <br />ji</td>
+<td>जी <br />jee</td>
+<td>जु <br />ju</td>
+<td>जू <br />joo</td>
+<td>जे <br />je</td>
+<td>जै <br />jai</td>
+<td>जो <br />jo</td>
+<td>जौ <br />jau</td>
+<td>जं <br />jan</td>
+<td>जः <br />jah</td>
+</tr>
+<tr style="background-color: #ffcc99;">
+<td>झ <br />jha</td>
+<td>झा <br />jha</td>
+<td>झि <br />jhi</td>
+<td>झी <br />jhee</td>
+<td>झु <br />jhu</td>
+<td>झू <br />jhoo</td>
+<td>झे <br />jhe</td>
+<td>झै <br />jhai</td>
+<td>झो <br />jho</td>
+<td>झौ <br />jhau</td>
+<td>झं <br />jhan</td>
+<td>झः <br />jhah</td>
+</tr>
+<tr style="background-color: #f2f2f2;">
+<td>ट <br />ta</td>
+<td>टा <br />ta</td>
+<td>टि <br />ti</td>
+<td>टी <br />tee</td>
+<td>टु <br />tu</td>
+<td>टू <br />too</td>
+<td>टे <br />te</td>
+<td>टै <br />tai</td>
+<td>टो <br />to</td>
+<td>टौ <br />tau</td>
+<td>टं <br />tan</td>
+<td>टः <br />tah</td>
+</tr>
+<tr style="background-color: #ffcc99;">
+<td>ठ <br />tha</td>
+<td>ठा <br />tha</td>
+<td>ठी <br />thi</td>
+<td>ठी <br />thee</td>
+<td>ठु <br />thu</td>
+<td>ठू <br />thoo</td>
+<td>ठे <br />the</td>
+<td>ठै <br />thai</td>
+<td>ठो <br />tho</td>
+<td>ठौ <br />thau</td>
+<td>ठं <br />than</td>
+<td>ठः <br />thah</td>
+</tr>
+<tr style="background-color: #f2f2f2;">
+<td>ड <br />da</td>
+<td>डा <br />da</td>
+<td>डि <br />di</td>
+<td>डी <br />dee</td>
+<td>डु <br />du</td>
+<td>डू<br /> doo</td>
+<td>डे <br />de</td>
+<td>डै <br />dai</td>
+<td>डो <br />do</td>
+<td>डौ <br />dau</td>
+<td>डं <br />dan</td>
+<td>डः <br />dah</td>
+</tr>
+<tr style="background-color: #ffcc99;">
+<td>ढ <br />dha</td>
+<td>ढा <br />dha</td>
+<td>ढि <br />dhi</td>
+<td>ढी <br />dhee</td>
+<td>ढु <br />dhu</td>
+<td>ढू <br />dhoo</td>
+<td>ढे <br />dhe</td>
+<td>ढै <br />dhai</td>
+<td>ढो <br />dho</td>
+<td>ढौ <br />dhau</td>
+<td>ढं <br />dhan</td>
+<td>ढः <br />dhah</td>
+</tr>
+<tr style="background-color: #f2f2f2;">
+<td>ण <br />na</td>
+<td>णा <br />na</td>
+<td>णि <br />ni</td>
+<td>णी <br />nee</td>
+<td>णु <br />nu</td>
+<td>णू <br />noo</td>
+<td>णे <br />ne</td>
+<td>णै <br />nai</td>
+<td>णो <br />no</td>
+<td>णौ <br />nau</td>
+<td>णं <br />nan</td>
+<td>णः <br />nah</td>
+</tr>
+<tr style="background-color: #ffcc99;">
+<td>त <br />ta</td>
+<td>ता <br />ta</td>
+<td>ति <br />ti</td>
+<td>ती <br />tee</td>
+<td>तु <br />tu</td>
+<td>तू <br />too</td>
+<td>ते <br />te</td>
+<td>तै <br />tai</td>
+<td>तो <br />to</td>
+<td>तौ <br />tau</td>
+<td>तं <br />tan</td>
+<td>तः <br />tah</td>
+</tr>
+<tr style="background-color: #f2f2f2;">
+<td>थ <br />tha</td>
+<td>था <br />tha</td>
+<td>थि <br />thi</td>
+<td>थी <br />thee</td>
+<td>थु <br />thu</td>
+<td>थू <br />thoo</td>
+<td>थे <br />the</td>
+<td>थै <br />thai</td>
+<td>थो <br />tho</td>
+<td>थौ <br />thau</td>
+<td>थं <br />than</td>
+<td>थः <br />thah</td>
+</tr>
+<tr style="background-color: #ffcc99;">
+<td>द <br />da</td>
+<td>दा <br />da</td>
+<td>दि <br />di</td>
+<td>दी <br />dee</td>
+<td>दु <br />du</td>
+<td>दू <br />doo</td>
+<td>दे <br />de</td>
+<td>दै <br />dai</td>
+<td>दो <br />do</td>
+<td>दौ <br />dau</td>
+<td>दं <br />dan</td>
+<td>दः <br />dah</td>
+</tr>
+<tr style="background-color: #f2f2f2;">
+<td>ध <br />dha</td>
+<td>धा <br />dha</td>
+<td>धि <br />dhi</td>
+<td>धी <br />dhee</td>
+<td>धु <br />dhu</td>
+<td>धू <br />dhoo</td>
+<td>धे <br />dhe</td>
+<td>धै <br />dhai</td>
+<td>धो <br />dho</td>
+<td>धौ <br />dhau</td>
+<td>धं <br />dhan</td>
+<td>धः <br />dhah</td>
+</tr>
+<tr style="background-color: #ffcc99;">
+<td>न <br />na</td>
+<td>ना <br />na</td>
+<td>नि <br />ni</td>
+<td>नी <br />nee</td>
+<td>नु <br />nu</td>
+<td>नू <br />noo</td>
+<td>ने <br />ne</td>
+<td>ने <br />nai</td>
+<td>नो <br />no</td>
+<td>नौ <br />nau</td>
+<td>नं <br />nan</td>
+<td>नः <br />nah</td>
+</tr>
+<tr style="background-color: #f2f2f2;">
+<td>प <br />pa</td>
+<td>पा <br />pa</td>
+<td>पि <br />pi</td>
+<td>पी <br />pee</td>
+<td>पु <br />pu</td>
+<td>पू <br />poo</td>
+<td>पे <br />pe</td>
+<td>पौ <br />pai</td>
+<td>पो <br />po</td>
+<td>पौ <br />pau</td>
+<td>पं <br />pan</td>
+<td>पः <br />pah</td>
+</tr>
+<tr style="background-color: #ffcc99;">
+<td>फ <br />pha</td>
+<td>फा <br />pha</td>
+<td>फि <br />phi</td>
+<td>फी <br />phee</td>
+<td>फु <br />phu</td>
+<td>फू <br />phoo</td>
+<td>फे <br />phe</td>
+<td>फै <br />phai</td>
+<td>फ़ो <br />pho</td>
+<td>फौ <br />phau</td>
+<td>फं <br />phan</td>
+<td>फः <br />phah</td>
+</tr>
+<tr style="background-color: #f2f2f2;">
+<td>ब <br />ba</td>
+<td>बा <br />ba</td>
+<td>बि <br />bi</td>
+<td>बी <br />bee</td>
+<td>बु <br />bu</td>
+<td>बू <br />boo</td>
+<td>बे <br />be</td>
+<td>बै <br />bai</td>
+<td>बो <br />bo</td>
+<td>बौ <br />bau</td>
+<td>बं <br />ban</td>
+<td>बः <br />bah</td>
+</tr>
+<tr style="background-color: #ffcc99;">
+<td>भ <br />bha</td>
+<td>भा <br />bha</td>
+<td>भि <br />bhi</td>
+<td>भी <br />bhee</td>
+<td>भु <br />bhu</td>
+<td>भू <br />bhoo</td>
+<td>भे <br />bhe</td>
+<td>भ <br />bhai</td>
+<td>भो <br />bho</td>
+<td>भौ <br />bhau</td>
+<td>भं <br />bhan</td>
+<td>भः <br />bhah</td>
+</tr>
+<tr style="background-color: #f2f2f2;">
+<td>म <br />ma</td>
+<td>मा <br />ma</td>
+<td>मि <br />mi</td>
+<td>मी <br />mee</td>
+<td>मु <br />mu</td>
+<td>मू <br />moo</td>
+<td>मे <br />me</td>
+<td>मै <br />mau</td>
+<td>मो <br />mo</td>
+<td>मौ <br />mau</td>
+<td>मं <br />man</td>
+<td>मः <br />mah</td>
+</tr>
+<tr style="background-color: #ffcc99;">
+<td>य <br />ya</td>
+<td>या <br />ya</td>
+<td>यि <br />yi</td>
+<td>यी <br />yee</td>
+<td>यु <br />yu</td>
+<td>यू <br />yoo</td>
+<td>ये <br />ye</td>
+<td>यै <br />yai</td>
+<td>यो <br />yo</td>
+<td>यौ <br />yau</td>
+<td>यं <br />yan</td>
+<td>यः <br />yah</td>
+</tr>
+<tr style="background-color: #f2f2f2;">
+<td>र <br />ra</td>
+<td>रा <br />ra</td>
+<td>रि <br />ri</td>
+<td>री <br />ree</td>
+<td>रु <br />ru</td>
+<td>रू <br />roo</td>
+<td>रे <br />re</td>
+<td>रै <br />rai</td>
+<td>रो <br />ro</td>
+<td>रौ <br />rau</td>
+<td>रं <br />ran</td>
+<td>रः <br />rah</td>
+</tr>
+<tr style="background-color: #ffcc99;">
+<td>ल <br />la</td>
+<td>ला <br />la</td>
+<td>लि <br />li</td>
+<td>ली <br />lee</td>
+<td>लु <br />lu</td>
+<td>लू <br />loo</td>
+<td>ले <br />le</td>
+<td>लै <br />lai</td>
+<td>लो <br />lo</td>
+<td>लौ <br />lau</td>
+<td>लं <br />lan</td>
+<td>लः <br />lah</td>
+</tr>
+<tr style="background-color: #f2f2f2;">
+<td>व <br />wa</td>
+<td>वा <br />wa</td>
+<td>वि <br />wi</td>
+<td>वी <br />wee</td>
+<td>वु <br />wu</td>
+<td>वू <br />woo</td>
+<td>वे <br />we</td>
+<td>वै <br />wai</td>
+<td>वो <br />wo</td>
+<td>वौ <br />wau</td>
+<td>वं <br />wan</td>
+<td>वः <br />wah</td>
+</tr>
+<tr style="background-color: #ffcc99;">
+<td>श <br />sha</td>
+<td>शा <br />sha</td>
+<td>शि <br />shi</td>
+<td>शी <br />shee</td>
+<td>शु <br />shu</td>
+<td>शू <br />shoo</td>
+<td>शे <br />she</td>
+<td>शै <br />shai</td>
+<td>शो <br />sho</td>
+<td>शौ <br />shau</td>
+<td>शं <br />shan</td>
+<td>शः <br />shah</td>
+</tr>
+<tr style="background-color: #f2f2f2;">
+<td>ष <br />sha</td>
+<td>षा <br />sha</td>
+<td>षि <br />shi</td>
+<td>षी <br />shee</td>
+<td>षु <br />shu</td>
+<td>षू <br />shoo</td>
+<td>षे <br />she</td>
+<td>षै <br />shai</td>
+<td>षो <br />sho</td>
+<td>षौ <br />shau</td>
+<td>षं <br />shan</td>
+<td>षः <br />shah</td>
+</tr>
+<tr style="background-color: #ffcc99;">
+<td>स <br />sa</td>
+<td>सा <br />sa</td>
+<td>सि <br />si</td>
+<td>सी <br />see</td>
+<td>सु <br />su</td>
+<td>सू <br />soo</td>
+<td>से <br />se</td>
+<td>सै <br />sai</td>
+<td>सो <br />so</td>
+<td>सौ <br />sau</td>
+<td>सं <br />san</td>
+<td>सः <br />sah</td>
+</tr>
+<tr style="background-color: #f2f2f2;">
+<td>ह <br />ha</td>
+<td>हा <br />ha</td>
+<td>हि <br />hi</td>
+<td>ही <br />hee</td>
+<td>हु <br />hu</td>
+<td>हू <br />hoo</td>
+<td>हे <br />he</td>
+<td>है <br />hai</td>
+<td>हो <br />ho</td>
+<td>हौ <br />hau</td>
+<td>हं <br />han</td>
+<td>हः <br />hah</td>
+</tr>
+<tr style="background-color: #ffcc99">
+<td>क्ष <br />ksha</td>
+<td>क्षा <br />ksha</td>
+<td>क्षि <br />kshi</td>
+<td>क्षी <br />kshee</td>
+<td>क्षु <br />kshu</td>
+<td>क्षू <br />kshoo</td>
+<td>क्षे <br />kshe</td>
+<td>क्षै <br />kshai</td>
+<td>क्षो <br />ksho</td>
+<td>क्षौ <br />kshau</td>
+<td>क्षं <br />kshan</td>
+<td>क्षः <br />kshah</td>
+</tr>
+<tr style="background-color: #f2f2f2;">
+<td>ज्ञ <br />gya</td>
+<td>ज्ञा <br />gya</td>
+<td>ज्ञि <br />gyi</td>
+<td>ज्ञी <br />gyee</td>
+<td>ज्ञु <br />gyu</td>
+<td>ज्ञू <br />gyoo</td>
+<td>ज्ञे <br />gye</td>
+<td>ज्ञै <br />gyai</td>
+<td>ज्ञो <br />gyo</td>
+<td>ज्ञौ <br />gyau</td>
+<td>ज्ञं <br />gyan</td>
+<td>ज्ञः <br />gyah</td>
+</tr>
+</table>
       </main>
 
       <Footer />
